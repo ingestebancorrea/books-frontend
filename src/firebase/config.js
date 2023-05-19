dab261eead27a30ce2819660bfe87177cff42511
Esteban Correa <estebancorrea405@gmail.com>
2023-05-19 15:16:19 -0500

    Creando variables de entorno para la configuración de Firabase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,17 +2,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAql3DRWE9loo8H7B83SvC705j8yI3tSZQ",
-  authDomain: "react-journal-cc109.firebaseapp.com",
-  projectId: "react-journal-cc109",
-  storageBucket: "react-journal-cc109.appspot.com",
-  messagingSenderId: "564773870489",
-  appId: "1:564773870489:web:18c0a407ee2b059d50d108"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
